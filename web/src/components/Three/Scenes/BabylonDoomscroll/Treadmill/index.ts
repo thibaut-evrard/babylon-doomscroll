ddcc997988b50014c07bbefde53083bcdd1b4352
thibaut evrard <thibaut.evrard.1509@gmail.com>
2025-02-19 08:02:20 +0000

    Setup next app and add basic classes

---
+++ web/src/components/Three/Scenes/BabylonDoomscroll/Treadmill/index.ts
@@ -0,0 +1,11 @@
+import {BoxGeometry, Mesh, MeshNormalMaterial, PlaneGeometry} from 'three';
+
+class Treadmill extends Mesh {
+  constructor() {
+    const geometry = new PlaneGeometry();
+    const material = new MeshNormalMaterial();
+    super(geometry, material);
+  }
+}
+
+export default Treadmill;
