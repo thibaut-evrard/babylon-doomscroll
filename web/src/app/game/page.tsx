d4d297bd03eb85ee94c28108abb31125628d7079
thibaut evrard <thibaut.evrard.1509@gmail.com>
2025-02-22 18:09:07 +0000

    Make application mobile only

--- web/src/app/game/page.tsx
+++ web/src/app/game/page.tsx
@@ -1,8 +1,13 @@
 'use client';
 import GameView from '@/components/GameView';
+import {useIsMobile} from '@/hooks/useIsMobile';
 
 const GamePage: React.FC = () => {
-  return <GameView />;
+  const isMobile = useIsMobile();
+
+  if (isMobile === undefined) return;
+  if (isMobile === false) return <h1>Mobile only</h1>;
+  if (isMobile === true) return <GameView />;
 };
 
 export default GamePage;
---
+++ web/src/hooks/useIsMobile.ts
@@ -0,0 +1,19 @@
+import {useEffect, useState} from 'react';
+
+const MOBILE_PX_MIN_WIDTH = 600;
+
+export const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined);
+
+  const updateIsMobile = () => {
+    console.log(window.innerWidth);
+    setIsMobile(window.innerWidth < MOBILE_PX_MIN_WIDTH);
+  };
+  useEffect(() => {
+    window.addEventListener('resize', updateIsMobile);
+    updateIsMobile();
+    return () => window.removeEventListener('resize', updateIsMobile);
+  }, []);
+
+  return isMobile;
+};
