b4b6e1fab1a7f906830d8b46afad4c52356167d1
thibaut evrard <thibaut.evrard.1509@gmail.com>
2025-03-09 12:38:28 +0000

    Remove end cta

--- web/src/components/Overlay/index.tsx
+++ web/src/components/Overlay/index.tsx
@@ -1,6 +1,5 @@
-import {FC, useEffect, useRef, useState} from 'react';
+import {FC, useRef} from 'react';
 import styles from './styles.module.scss';
-import EndCta from './EndCta';
 import {useOnScroll} from '@/hooks/useOnScroll';
 import Trophy from './Trophy';
 
@@ -11,15 +10,12 @@ interface Props {
   onEnd: () => void;
 }
 
-const SCROLL_CHECK_INTERVAL = 500;
 const TROPHIES = CONTENT_SERVICE.trophies;
 
-const Overlay: FC<Props> = ({onEnd}) => {
+const Overlay: FC<Props> = () => {
   const {trophy, setTrophy} = useGameStore();
   const progressBarRef = useRef<HTMLDivElement>(null);
-  const previousScrollRef = useRef(0);
   const nextTrophyRef = useRef(TROPHIES[0]);
-  const [isStationary, setIsStationary] = useState(false);
 
   useOnScroll(() => {
     const nextTrophy = nextTrophyRef.current;
@@ -38,25 +34,10 @@ const Overlay: FC<Props> = ({onEnd}) => {
     }
   }, [trophy]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (window.scrollY < 1000) return;
-      if (previousScrollRef.current === window.scrollY) {
-        setIsStationary(true);
-      } else {
-        setIsStationary(false);
-      }
-      previousScrollRef.current = window.scrollY;
-    }, SCROLL_CHECK_INTERVAL);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className={styles.overlay_container}>
       <div className={styles.progress} ref={progressBarRef} />
       {trophy && <Trophy content={trophy} key={trophy.title} />}
-      <EndCta isVisible={isStationary} onClick={onEnd} />
     </div>
   );
 };
